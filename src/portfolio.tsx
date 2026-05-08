bfd18c07de4227656b3bc690b6c148678c7f2a9e
yangyeeun030 <20222030@edu.hanbat.ac.kr>
2026-05-08 20:32:05 +0900

    fork components

---
+++ src/portfolio.tsx
@@ -0,0 +1,149 @@
+import emoji from "react-easy-emoji";
+
+export const greeting = {
+  username: "Username",
+  title: "Hello I'm Username",
+  subTitle: emoji("subtitle ❤"),
+  role: "Your role",
+  resumeLink: "Your resume link... google drive or something else",
+  // IMPORTANT
+  // If you don't want to show this, change view to false. DO NOT DELETE!
+  view: true,
+};
+
+export const socialMediaLinks = {
+  // IMPORTANT
+  // if you don't have, change value to blank or false!! DO NOT DELETE!
+  github: "Your github link",
+  linkedin: "Your linkedin link",
+  email: "your email link",
+  facebook: "Your facebook link",
+  twitter: "Your twitter link",
+  instagram: "Your instagram link",
+  medium: "Your medium link",
+  stackoverflow: "Your stackoverflow link",
+};
+
+export const skills = {
+  title: emoji("I Can Do ⚡"),
+  subTitle: "subtitle for skill",
+  describeSkills: [
+    emoji("✔ describe first"),
+    emoji("✔ describe second"),
+    emoji("✔ describe third"),
+    emoji("✔ describe forth"),
+    emoji("✔ describe fifth"),
+  ],
+  // IMPORTANT
+  // You need to find className for font-awesome icon
+  // Go to https://fontawesome.com/ and search icon
+  lists: [
+    { fontAwesome: "fab fa-html5", text: "HTML5", proficiency: 80 },
+    { fontAwesome: "fab fa-css3-alt", text: "CSS3", proficiency: 70 },
+    { fontAwesome: "fab fa-js", text: "JS", proficiency: 70 },
+    { fontAwesome: "fab fa-sass", text: "Sass", proficiency: 30 },
+    { fontAwesome: "fab fa-react", text: "React", proficiency: 60 },
+    { fontAwesome: "fab fa-aws", text: "AWS", proficiency: 20 },
+  ],
+  view: true,
+};
+
+export const experience = {
+  title: "Work Experience",
+  lists: [
+    {
+      date: "2018.03~",
+      company: "Google",
+      role: "Front-End Developer",
+    },
+    {
+      date: "test",
+      company: "test",
+      role: "test",
+    },
+    {
+      date: "test",
+      company: "test",
+      role: "test",
+    },
+    {
+      date: "test",
+      company: "test",
+      role: "test",
+    },
+    {
+      date: "test",
+      company: "test",
+      role: "test",
+    },
+  ],
+  view: true,
+};
+
+export const openSourceProjects = {
+  title: emoji("Open Source Projects 💻"),
+  view: true,
+};
+
+export const projects = {
+  title: emoji("Projects 💻"),
+  subTitle: "subtitle for projects",
+  lists: [
+    {
+      title: "Project1",
+      desc: "very very very very very very very very very very very very very very long very long description for clamp testing",
+      url: "#",
+    },
+    {
+      title: "Project2",
+      desc: "description",
+      url: "#",
+    },
+  ],
+  view: true,
+};
+
+export const achievements = {
+  title: emoji("Achievements And Certifications 🏆"),
+  subTitle: "subtitle for achievement",
+  lists: [
+    {
+      img: require("./assets/sad-icon.png"),
+      alt: "I'm sad..",
+      title: "There is no Achievement...😥",
+      desc: "I'll try harder!",
+    },
+    {
+      img: require("./assets/sad-icon.png"),
+      alt: "I'm sad..",
+      title: "There is no Certification...😥",
+      desc: "I'll try harder!",
+    },
+  ],
+  view: true,
+};
+
+export const blogs = {
+  title: emoji("My Posts 📰"),
+  subTitle: "subtitle for posts",
+  lists: [
+    {
+      title: "Your Post1",
+      desc: "Description",
+      url: "#",
+    },
+    {
+      title: "Your post2",
+      desc: "Description",
+      url: "#",
+    },
+  ],
+  view: true,
+};
+
+export const contactInfo = {
+  title: "Contact To Me! 📞",
+  subTitle: "subtitle for contact",
+  introduce: emoji("your introduce❤"),
+  view: true,
+};
